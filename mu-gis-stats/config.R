8210d7611ceb08433d5b88dae8582c457d3b0326
Dylan Beaudette <dylan.beaudette@gmail.com>
2016-04-07 16:07:00 -0700

    adding curvature classification, fixing style problems, more customization

--- mu-gis-stats/config.R
+++ mu-gis-stats/config.R
@@ -1,32 +1,28 @@
 ### MU GIS Summary Report
-### 2016-03-22
+### 2016-04-07
 ### D.E. Beaudette and J. Wood
 ###
 ### configuration file, edit as needed
 
 
-## data sources
-
-# raster data
-raster.list <- list(`Mean Annual Air Temperature (deg C)`=raster('E:/gis_data/prism/final_MAAT_800m.tif'), 
+### raster data
+raster.list <- list(`Mean Annual Air Temperature (degrees C)`=raster('E:/gis_data/prism/final_MAAT_800m.tif'), 
                     `Mean Annual Precipitation (mm)`=raster('E:/gis_data/prism/final_MAP_mm_800m.tif'),
                     `Effective Precipitation (mm)`=raster('E:/gis_data/prism/effective_precipitation_800m.tif'),
                     `Frost-Free Days`=raster('E:/gis_data/prism/ffd_mean_800m.tif'),
-                    `Growing Degree Days (deg C)`=raster('E:/gis_data/prism/gdd_mean_800m.tif'),
-                    `Elevation (m)`=raster('E:/gis_data/region-2-mu-analysis/elev_30.tif'),
-                    `Slope Gradient (%)`=raster('E:/gis_data/region-2-mu-analysis/slope_30.tif'),
+                    `Growing Degree Days (degrees C)`=raster('E:/gis_data/prism/gdd_mean_800m.tif'),
+                    `Elevation (m)`=raster('E:/gis_data/ca630/ca630_elev/hdr.adf'),
+                    `Slope Gradient (%)`=raster('E:/gis_data/ca630/ca630_slope/hdr.adf'),
                     `Annual Beam Radiance (MJ/sq.m)`=raster('E:/gis_data/ca630/beam_rad_sum_mj_30m.tif'),
-                    `(Estimated) MAST (deg C)`=raster('E:/gis_data/ca630/mast-model.tif'),
-                    `Slope Aspect (degrees)`=raster('E:/gis_data/region-2-mu-analysis/aspect_30.tif'),
-                    `Geomorphons`=raster('E:/gis_data/region-2-mu-analysis/forms30_region2.tif')
+                    `(Estimated) MAST (degrees C)`=raster('E:/gis_data/ca630/mast-model.tif'),
+                    `Slope Aspect (degrees)`=raster('E:/gis_data/ca630/aspect_ca630/hdr.adf'),
+                    `Geomorphon Landforms`=raster('E:/gis_data/region-2-mu-analysis/forms30_region2.tif'),
+                    `Curvature Classes`=raster('E:/gis_data/region-2-mu-analysis/curvature_classes_30_class_region2.tif')
 )
-
-### TODO Data from SDA: add from tutorial ...
-### 
 
 
 ### Data are in a large geodatabase with many map units, explicit subsetting
-
+# 
 # geodatabase path
 mu.dsn <- 'E:/gis_data/ca630/FG_CA630_OFFICIAL.gdb'
 # name of featureclass
@@ -34,9 +30,7 @@ mu.layer <- 'ca630_a'
 
 mu <-  readOGR(dsn=mu.dsn, layer=mu.layer, encoding='OpenFileGDB', stringsAsFactors = FALSE)
 # map unit symbols / keys to extract
-mu.set <- c('8115', '8175', '8176', '8177', '8178', '8179')
-
-###
+mu.set <- c('5012', '5015', '5201', '7085', '7089', '7011')
 
 
 # ### Typical SDJR style data: SHP with multiple map units
@@ -54,10 +48,13 @@ mu.set <- c('8115', '8175', '8176', '8177', '8178', '8179')
 ### column with map unit ID / key / symbol
 mu.col <- 'MUSYM'
 
-### do geomorphon summary?
-do.geomorphons <- TRUE
 
 ### polygon sampling density (samples / acre / polygon)
-# using too-small a sampling density will result in variation between runs
+# values less < 1 (coarse sampling density) will result in variation between runs, and un-sampled polygons
+# values > 5-10 will result in longer report run times
 pts.per.acre <- 1
+
+### quantiles of interest
+p.quantiles <- c(0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95)
+
 
