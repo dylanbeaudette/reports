78b62a6fa63194ffe200b3d9e8e3871a9eaab1ac
Dylan Beaudette <dylan.beaudette@gmail.com>
2016-06-22 11:44:52 -0700

    bug fixes and clarification

--- mu-gis-stats/config.R
+++ mu-gis-stats/config.R
@@ -6,43 +6,56 @@
 
 
 ### raster data
+# raster.list <- list(`Mean Annual Air Temperature (degrees C)`=raster('E:/gis_data/prism/final_MAAT_800m.tif'), 
+#                     `Mean Annual Precipitation (mm)`=raster('E:/gis_data/prism/final_MAP_mm_800m.tif'),
+#                     `Effective Precipitation (mm)`=raster('E:/gis_data/prism/effective_precipitation_800m.tif'),
+#                     `Frost-Free Days`=raster('E:/gis_data/prism/ffd_mean_800m.tif'),
+#                     `Growing Degree Days (degrees C)`=raster('E:/gis_data/prism/gdd_mean_800m.tif'),
+#                     `Elevation (m)`=raster('E:/gis_data/region-2-mu-analysis/elev_30.tif'),
+#                     `Slope Gradient (%)`=raster('E:/gis_data/region-2-mu-analysis/slope_30.tif'),
+#                     `Annual Beam Radiance (MJ/sq.m)`=raster('E:/gis_data/ca630/beam_rad_sum_mj_30m.tif'),
+#                     `(Estimated) MAST (degrees C)`=raster('E:/gis_data/ca630/mast-model.tif'),
+#                     `Slope Aspect (degrees)`=raster('E:/gis_data/region-2-mu-analysis/aspect_30.tif'),
+#                     `Geomorphon Landforms`=raster('E:/gis_data/region-2-mu-analysis/forms30_region2.tif'),
+#                     `Curvature Classes`=raster('E:/gis_data/region-2-mu-analysis/curvature_classes_30_class_region2.tif')
+# )
+
 raster.list <- list(`Mean Annual Air Temperature (degrees C)`=raster('E:/gis_data/prism/final_MAAT_800m.tif'), 
                     `Mean Annual Precipitation (mm)`=raster('E:/gis_data/prism/final_MAP_mm_800m.tif'),
                     `Effective Precipitation (mm)`=raster('E:/gis_data/prism/effective_precipitation_800m.tif'),
                     `Frost-Free Days`=raster('E:/gis_data/prism/ffd_mean_800m.tif'),
                     `Growing Degree Days (degrees C)`=raster('E:/gis_data/prism/gdd_mean_800m.tif'),
-                    `Elevation (m)`=raster('E:/gis_data/ca630/ca630_elev/hdr.adf'),
-                    `Slope Gradient (%)`=raster('E:/gis_data/ca630/ca630_slope/hdr.adf'),
-                    `Annual Beam Radiance (MJ/sq.m)`=raster('E:/gis_data/ca630/beam_rad_sum_mj_30m.tif'),
-                    `(Estimated) MAST (degrees C)`=raster('E:/gis_data/ca630/mast-model.tif'),
-                    `Slope Aspect (degrees)`=raster('E:/gis_data/ca630/aspect_ca630/hdr.adf'),
+                    `Elevation (m)`=raster('E:/gis_data/region-2-mu-analysis/elev_30.tif'),
+                    `Slope Gradient (%)`=raster('E:/gis_data/region-2-mu-analysis/slope_30.tif'),
+                    `Slope Aspect (degrees)`=raster('E:/gis_data/region-2-mu-analysis/aspect_30.tif'),
                     `Geomorphon Landforms`=raster('E:/gis_data/region-2-mu-analysis/forms30_region2.tif'),
                     `Curvature Classes`=raster('E:/gis_data/region-2-mu-analysis/curvature_classes_30_class_region2.tif')
 )
 
 
-### Data are in a large geodatabase with many map units, explicit subsetting
-# 
-# geodatabase path
-mu.dsn <- 'E:/gis_data/ca630/FG_CA630_OFFICIAL.gdb'
-# name of featureclass
-mu.layer <- 'ca630_a'
 
-mu <-  readOGR(dsn=mu.dsn, layer=mu.layer, encoding='OpenFileGDB', stringsAsFactors = FALSE)
-# map unit symbols / keys to extract
-mu.set <- c('5012', '5015', '5201', '7085', '7089', '7011')
-
-
-# ### Typical SDJR style data: SHP with multiple map units
+# ### Data are in a large geodatabase with many map units, explicit subsetting
 # 
-# # path to SHP
-# mu.dsn <- 'E:/gis_data/region-2-mu-analysis'
-# # SHP name, without file extension
-# mu.layer <- 'CA630-SDJR-example'
-# # load data
-# mu <-  readOGR(dsn=mu.dsn, layer=mu.layer, stringsAsFactors = FALSE)
+# # geodatabase path
+# mu.dsn <- 'E:/gis_data/ca630/FG_CA630_OFFICIAL.gdb'
+# # name of featureclass
+# mu.layer <- 'ca630_a'
 # 
-# ###
+# mu <-  readOGR(dsn=mu.dsn, layer=mu.layer, encoding='OpenFileGDB', stringsAsFactors = FALSE)
+# # map unit symbols / keys to extract
+# mu.set <- c('5012', '5015', '5201', '7085', '7089', '7011')
+# 
+
+### Typical SDJR style data: SHP with multiple map units
+
+# path to SHP
+mu.dsn <- 'testing'
+# SHP name, without file extension
+mu.layer <- 'Mus_for_analysis'
+# load data
+mu <-  readOGR(dsn=mu.dsn, layer=mu.layer, stringsAsFactors = FALSE)
+
+###
 
 
 ### column with map unit ID / key / symbol
@@ -51,8 +64,8 @@ mu.col <- 'MUSYM'
 
 ### polygon sampling density (samples / acre / polygon)
 # values less < 1 (coarse sampling density) will result in variation between runs, and un-sampled polygons
-# values > 5-10 will result in longer report run times
-pts.per.acre <- 1
+# values > 10 will result in longer report run times
+pts.per.acre <- 2
 
 ### quantiles of interest
 p.quantiles <- c(0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95)
--- mu-gis-stats/load-packages.R
+++ mu-gis-stats/load-packages.R
@@ -1,11 +1,16 @@
 ## you need all of these packages to run the report
 
+## get an older version of knitr
+## work-around, until we get R 3.3.0
+packageurl <- "http://cran.r-project.org/src/contrib/Archive/knitr/knitr_1.12.tar.gz"
+install.packages(packageurl, repos=NULL, type="source")
+
 # packages + deps from CRAN
-packages.to.get <- c('knitr', 'rmarkdown', 'rgdal', 'raster', 'plyr', 'reshape2', 'aqp', 'soilDB', 'sharpshootR', 'latticeExtra', 'clhs')
+packages.to.get <- c('rmarkdown', 'rgdal', 'raster', 'plyr', 'reshape2', 'aqp', 'soilDB', 'sharpshootR', 'latticeExtra', 'clhs', 'devtools')
 res <- sapply(packages.to.get, install.packages, dep=TRUE)
 
-# latest versions from r-forge
-install.packages("aqp", repos="http://R-Forge.R-project.org", type="source")
-install.packages("soilDB", repos="http://R-Forge.R-project.org", type="source")
-install.packages("sharpshootR", repos="http://R-Forge.R-project.org", type="source")
+# latest versions from GitHub
+devtools::install_github("ncss-tech/aqp", dependencies=FALSE, upgrade_dependencies=FALSE)
+devtools::install_github("ncss-tech/soilDB", dependencies=FALSE, upgrade_dependencies=FALSE)
+devtools::install_github("ncss-tech/sharpshootR", dependencies=FALSE, upgrade_dependencies=FALSE)
 
